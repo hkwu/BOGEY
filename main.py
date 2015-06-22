5a9e70446f94886c694f53eb334b57bbaea375d0
hkwu <kelvinwu1996@gmail.com>
2015-06-21 21:19:59 -0400

    Adding a little flair.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+##############################################
+# BOGEY
+# Created by Kelvin Wu
+# Initial commit 2015-06-13
+# Based on Jotaf's Complete Roguelike Tutorial
+##############################################
+
 import math
 import random
 import textwrap
